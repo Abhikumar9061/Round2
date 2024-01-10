92f07ecdc610f417753a130610ceea92125aa1f1
Abhikumar9061 <91717641+Abhikumar9061@users.noreply.github.com>
2024-01-10 14:56:01 +0530

    Add files via upload

---
+++ .history/public/server_20240110125926.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+  "mongodb+srv://abhinandankumar07102002:SojhdXDlIC1C0iHN@cluster0.efj7jtb.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Create a schema and model for users
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("images"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/style.css", (req, res) => {
+  res.type("text/css");
+  res.sendFile(__dirname + "/style.css");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+    if (user) {
+      res.send("Login successful!");
+    } else {
+      res.send("Invalid username or password");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.send("Username already exists");
+    } else {
+      const newUser = new User({ username, email, password });
+      await newUser.save();
+      res.send("Registration successful!");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
